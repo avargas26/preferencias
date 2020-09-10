7a53a2b4866c287adae457f3af6541a267b9808d
Andrés Vargas <andvargas25@gmail.com>
2020-09-10 01:25:14 -0300

    Se termina modulo prefenrecias para el usuario

---
+++ lib/src/share_prefs/preferencias_usuarios.dart
@@ -0,0 +1,54 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PreferenciasUsuario {
+  //patron singleton
+  static final PreferenciasUsuario _instancia =
+      new PreferenciasUsuario._internal();
+
+  //constrctor factory
+  factory PreferenciasUsuario() {
+    return _instancia;
+  }
+  PreferenciasUsuario._internal();
+  SharedPreferences _prefs;
+
+  initPrefs() async {
+    this._prefs = await SharedPreferences.getInstance();
+  }
+
+  //get y set de genero
+  get genero {
+    return _prefs.getInt('genero') ?? 1;
+  }
+
+  set genero(int value) {
+    _prefs.setInt('genero', value);
+  }
+
+  //get y set color
+  get colorSecundario {
+    return _prefs.getBool('colorSecundario') ?? false;
+  }
+
+  set colorSecundario(bool value) {
+    _prefs.setBool('colorSecundario', value);
+  }
+
+  //get y set nombre
+  get nombreUsuario {
+    return _prefs.getString('nombreUsuario') ?? '';
+  }
+
+  set nombreUsuario(String value) {
+    _prefs.setString('nombreUsuario', value);
+  }
+
+  //set get ultima pagina
+  get ultimaPagina {
+    return _prefs.getString('ultimaPagina') ?? 'home';
+  }
+
+  set ultimaPagina(String value) {
+    _prefs.setString('ultimaPagina', value);
+  }
+}
